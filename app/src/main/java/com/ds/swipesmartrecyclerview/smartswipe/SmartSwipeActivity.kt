5f166ebc0aaf914c5be36d94c02b09f11f2b020f
developersanchez <developersanchez1903@gmail.com>
2021-07-31 16:59:13 +0300

    refactored SmartSwipeActivity

--- app/src/main/java/com/ds/swipesmartrecyclerview/smartswipe/SmartSwipeActivity.kt
+++ app/src/main/java/com/ds/swipesmartrecyclerview/smartswipe/SmartSwipeActivity.kt
@@ -6,13 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.ds.swipesmartrecyclerview.R
 import com.ds.swipesmartrecyclerview.databinding.ActivitySmartSwipeBinding
 import com.ds.swipesmartrecyclerview.model.ItemModel
+import com.ds.swipesmartrecyclerview.toast
 import smartadapter.SmartRecyclerAdapter
 import smartadapter.diffutil.DiffUtilExtension
 import smartadapter.diffutil.SimpleDiffUtilExtension
 import smartadapter.viewevent.listener.OnClickEventListener
 
 class SmartSwipeActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySmartSwipeBinding
+    private lateinit var binding: ActivitySmartSwipeBinding
     lateinit var adapter: SmartRecyclerAdapter
 
     private val predicate = object : DiffUtilExtension.DiffPredicate<ItemModel> {
@@ -35,24 +36,17 @@ class SmartSwipeActivity : AppCompatActivity() {
     private fun initAdapter() {
         adapter = SmartRecyclerAdapter.items(getItems())
             .map(ItemModel::class, SmartSwipeViewHolder::class)
-            .setViewTypeResolver { item, position ->
-                when (item) {
-                    is ItemModel -> SmartSwipeViewHolder::class
-                    else -> null
-                }
-            }
-            .setLayoutManager(LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false).apply {
-                isItemPrefetchEnabled = true
-                initialPrefetchItemCount = 20
-            })
             .add(OnClickEventListener(SmartSwipeViewHolder::class, R.id.btnRemoveFromList) {
-
+                val item = it.adapter.getItem(it.position) as ItemModel
+                toast("removed ${item.title}")
             })
             .add(OnClickEventListener(SmartSwipeViewHolder::class, R.id.btnMove) {
-
+                val item = it.adapter.getItem(it.position) as ItemModel
+                toast("moved ${item.title}")
             })
             .add(OnClickEventListener(SmartSwipeViewHolder::class, R.id.btnSendOrder) {
-
+                val item = it.adapter.getItem(it.position) as ItemModel
+                toast("sent order ${item.title}")
             })
             .add(SimpleDiffUtilExtension(predicate))
             .add(SmartSwipeAdapterBinder(R.id.rootSwipe, true))
